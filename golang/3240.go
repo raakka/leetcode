c1d439b90e96db8360b3e158ffda73ca88c3d6ea
XVI <jrip03@gmail.com>
2020-12-04 16:12:41 -0500

    Update 3240.go

--- golang/3240.go
+++ golang/3240.go
@@ -1,8 +1,17 @@
-func sortedSquares(nums []int) []int {
+func sortedSquares(nums []int) []int {  
+    var temp int
+    
     for i := 0; i < len(nums); i++ {
         nums[i] = nums[i]*nums[i]
     }
-    for i := 0; i < len(nums); i++ {
-
+    for j := 0; j < len(nums); j++ {
+        for l := j + 1; l < len(nums); l++ {
+            if nums[l] < nums[j] {
+                temp = nums[j]
+                nums[j] = nums[l]
+                nums[l] = temp
+            }           
+        }
     }
+    return nums
 }
